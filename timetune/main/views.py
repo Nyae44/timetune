7af9b8c2fcf122fe7e0c4ff878c949af40be56d5
Nyae44 <tingadaley@gmail.com>
2024-05-10 20:50:14 +0300

    add navigation bar

--- timetune/main/views.py
+++ timetune/main/views.py
@@ -1,7 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .forms import CreateUserForm, LoginForm
 # Create your views here.
 def home(request):
     
     
     return render(request, 'main/index.html')
+
+# Register a user
+def register(request):
+    form = CreateUserForm()
+    
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+        
+        
+        if form.is_valid():
+            form.save()
+            
+        # return redirect()
+    context = {'form':form}
+    
+    return render(request, "main/register.html", context=context)
+
+# Login a user
