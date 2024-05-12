71aca1bc84cdd7adedadd5a677fe637f5a6b25ba
Nyae44 <tingadaley@gmail.com>
2024-05-12 18:00:40 +0300

    add login view

--- timetune/main/views.py
+++ timetune/main/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .forms import CreateUserForm, LoginForm
-# Create your views here.
+
+from django.contrib.auth.models import auth
+from django.contrib.auth import authenticate
 def home(request):
     
     
@@ -24,3 +26,26 @@ def register(request):
     return render(request, "main/register.html", context=context)
 
 # Login a user
+
+def login(request):
+    form = LoginForm()
+    
+    if request.method == "POST":
+        form = LoginForm(request, data=request.POST)
+        
+        if form.is_valid():
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            
+            user = authenticate(request, username=username, password=password)
+            
+            # Check if the user exists
+            
+            if user is not None:
+                
+                auth.login(request, user)
+                
+                # return redirect('')
+    context={'form':form}
+    
+    return render(request, "main/login.html", context=context)
